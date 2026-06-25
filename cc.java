83e4090a458a1d23b4ca5b7b7cd3f02980491d65
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-25 20:22:35 +0600

    Add Jam class with default constructor

---
+++ cc.java
@@ -0,0 +1,12 @@
+class Jam {
+
+    Jam() {
+        System.out.println("Default Constructor Called");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Jam obj = new Jam();
+    }
+}
