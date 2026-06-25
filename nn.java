c806d299807849f15265e7421fd84280fd029141
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-25 20:28:35 +0600

    Add Nur class and main method for instantiation

---
+++ nn.java
@@ -0,0 +1,12 @@
+class Nur {
+
+    Nur(String name) {
+        System.out.println("Name: " + name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Nur obj = new Nur("Jamsad");
+    }
+}
