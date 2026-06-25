a1cc9e90bd8bbce325dba0e4ce3e793bf02906b8
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-25 20:37:46 +0600

    Add Nure class and main method for demonstration

---
+++ nu.java
@@ -0,0 +1,13 @@
+class Nure {
+
+    Nure(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Nure obj = new Nure("Jannat", 22);
+    }
+}
