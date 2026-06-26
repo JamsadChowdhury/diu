5ec0757bcadf54a373930585a05884b95687f4bc
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-26 20:39:41 +0600

    Add Main class with ArrayList example

---
+++ ab.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> jannat = new ArrayList<>();
+
+        jannat.add("Apple");
+        jannat.add("Mango");
+        jannat.add("Orange");
+
+        System.out.println(jannat.get(1));
+    }
+}
