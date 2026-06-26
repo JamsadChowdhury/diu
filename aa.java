dc16609ee912813b1330d1225712c34201c9c89d
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-26 20:37:36 +0600

    Create aa.java

---
+++ aa.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> nure = new ArrayList<>();
+
+        nure.add("Jam");
+        nure.add("Nur");
+        nure.add("Jannat");
+
+        for (String name : nure) {
+            System.out.println(name);
+        }
+    }
+}
