5a702f5ce8a6b32701cb4faef40a7424c949f6ef
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-26 20:14:18 +0600

    Add Main class with ArrayList example

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> jam = new ArrayList<>();
+
+        jam.add("Jamsad");
+        jam.add("Mahin");
+        jam.add("Jannat");
+
+        System.out.println(jam);
+    }
+}
