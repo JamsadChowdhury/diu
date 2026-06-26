3becfa13f8ce2da5cc770e556dafc7f712d5c734
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-26 20:20:10 +0600

    Add Main class with ArrayList example

---
+++ ar.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nur = new ArrayList<>();
+
+        nur.add(10);
+        nur.add(20);
+        nur.add(30);
+
+        System.out.println(nur);
+    }
+}
