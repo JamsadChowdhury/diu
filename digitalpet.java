8e041592a98c44cca6ce6c942bb5e4a645a525cd
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-28 21:13:40 +0600

    Add DigitalPet class with state management

---
+++ digitalpet.java
@@ -0,0 +1,23 @@
+public class DigitalPet {
+
+    public static void main(String[] args) {
+
+        String state = "Happy";
+
+        System.out.println("Current State : " + state);
+
+        state = "Sad";
+        System.out.println("Punishment -> " + state);
+
+        state = "Happy";
+        System.out.println("Praise -> " + state);
+
+        state = "Sad";
+        System.out.println("Punishment -> " + state);
+
+        state = "Heart Broken";
+        System.out.println("Punishment Again -> " + state);
+
+    }
+
+}
