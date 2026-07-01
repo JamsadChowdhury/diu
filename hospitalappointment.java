5ad024ff279e2eeb5f2a5ed38b687b09e856543b
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-07-01 20:27:04 +0600

    Add Hospital class with appointment confirmation logic

---
+++ hospitalappointment.java
@@ -0,0 +1,18 @@
+public class Hospital {
+
+    public static void main(String[] args) {
+
+        boolean doctorAvailable = true;
+
+        if(doctorAvailable){
+
+            System.out.println("Appointment Confirmed");
+
+        }else{
+
+            System.out.println("Choose Another Doctor");
+        }
+
+    }
+
+}
