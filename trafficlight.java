a6b8590c2eaf1a7076d8a078755db58134419330
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-07-03 21:49:49 +0600

    Add TrafficLight class to display light colors

---
+++ trafficlight.java
@@ -0,0 +1,16 @@
+public class TrafficLight {
+
+    public static void main(String[] args) {
+
+        String light = "Red";
+
+        System.out.println(light);
+
+        light = "Yellow";
+        System.out.println(light);
+
+        light = "Green";
+        System.out.println(light);
+    }
+
+}
